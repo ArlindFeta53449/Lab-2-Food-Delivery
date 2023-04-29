cfbe4e9e80934110862a2a25d10d3b1932b7f77e
AltinMurtezaj <93322564+AltinMurtezaj@users.noreply.github.com>
2023-04-29 18:02:58 +0200

    Added some atributes at Restaurant

--- UserMicroservice/Data/Data/DTOs/Restaurant/RestaurantCreateDto.cs
+++ UserMicroservice/Data/Data/DTOs/Restaurant/RestaurantCreateDto.cs
@@ -11,5 +11,6 @@ namespace Data.DTOs
         public string Name { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
+        public string Image { get; set; }
     }
 }
